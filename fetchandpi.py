4955d24f533ac98db5bdc9ef130a0a9556c9b860
Błażej Cegiełka <blazej.cegielka@gmail.com>
2018-11-27 14:35:35 +0000

    Added anothre pi approx algorithm

--- fetchandpi.py
+++ fetchandpi.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+from decimal import Decimal as Dec
+
 import gevent
 from gevent import Greenlet
 from gevent.pool import Pool
@@ -16,35 +18,62 @@ def task(evt, pid):
     print('Task %s done. Took %s' % (pid, took))
 
 
-def loop(evt):
-    i = 0
+def pi_approx_classic(evt):
+    S, i, sign = 0, 0, 1
+
+    def pi(ppi):
+        return 4*ppi
+
     while True:
+        print('Pi iteration: %s, %s' % (i, pi(S)))
+        S += Dec(float(sign) / (2*i+1))
+        evt.set(pi(S))
+
         i += 1
-        evt.set(i)
-        print('Pi iteration: %s' % i)
+        sign = -sign
 
         # making sure gevent yields
-        # time.sleep(1)
-        gevent.sleep(1)
+        gevent.sleep(0)
 
-    return i
+
+def pi_approx_ng(evt):
+    K, M, L, X, S, i = 6, 1, 13591409, 1, 13591409, 1
+
+    def pi(ppi):
+        return 426880 * Dec(10005).sqrt() / ppi
+
+    while True:
+        print('Pi iteration: %s, %s' % (i, pi(S)))
+        M = (K**3 - 16*K) * M // i**3
+        L += 545140134
+        X *= -262537412640768000
+
+        S += Dec(M * L) / X
+        evt.set(pi(S))
+
+        K += 12
+        i += 1
+
+        # making sure gevent yields
+        gevent.sleep(0)
 
 
 def run(pool, evt):
     for i in xrange(10):
         pool.start(Greenlet(task, evt, i))
 
-    pi = Greenlet(loop, evt)
+    pi = Greenlet(pi_approx_classic, evt)
     pool.start(pi)
 
     while True:
-        gevent.sleep(1)
         print('Pool size: %s' % len(pool))
         if len(pool) == 1 and pi in pool:
             print('I should die: %s' % evt.get())
             pool.killone(pi)
             print('and i died: %s' % evt.get())
             return
+
+        gevent.sleep(0)
 
 
 def main():
